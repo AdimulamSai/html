395b5eb4791b1104a77977d9422a1569d70ab09f
Adimulam sai koteswara rao <adimulamsai584@gmail.com>
2021-01-20 13:45:01 +0530

    19jan

---
+++ 19.js
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <form>
+            <input type="text" id="test"></input>
+            <button onclick="changetest()">add</button>
+        </form>
+        <ul id="tests"></ul>
+        <script>
+          function changetest()
+        </script>
+    </body>
+</html>
